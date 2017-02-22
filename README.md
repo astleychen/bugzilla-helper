75d08c59956c57efbb84db4164c3beeae016c4a1
Astley Chen <aschen@mozilla.com>
2017-02-22 16:07:42 +0800

    Initial commit to support Copy2Wiki function in Firefox WebExtension.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bugzilla-helper
-A WebExtension used as a helper for Mozilla Bugzilla pages.
+A Firefox WebExtension used as a helper on Mozilla Bugzilla bug pages.
+
+## Features
+* Copy2Wiki - with a button attached with bug summary so that you are able to copy bug summary to wiki-formatted string.
+
+## Changelog
+* 2017/02/21 - Initial version converted from Greasemonkey user script. [Originated with @mephisto41]
+* 2017/02/22 - Workaround copy-to-clipboard function as replacement of GM_setClipboard. [(Bug 1334174)](https://bugzilla.mozilla.org/show_bug.cgi?id=1334174#c8)
---
+++ summary2wiki.js
@@ -0,0 +1,36 @@
+"use strict"; 
+// ==UserScript==
+// @name        summary2wiki
+// @namespace   bugzilla
+// @description Copy bug title to clipboard with wiki type.
+// @include     https://bugzilla.mozilla.org/show_bug.cgi?id=*
+// @version     1
+// @grant       GM_setClipboard
+// ==/UserScript==
+var title = document.querySelector(".bz_alias_short_desc_container")
+
+var button = document.createElement("input")
+button.type = "button"
+button.value = "Copy2Wiki"
+title.appendChild(button)
+
+button.onclick = function(e) {
+  var bugid = document.querySelector(".bz_alias_short_desc_container > a > b")
+  var bugdesc = document.querySelector("#short_desc_nonedit_display")
+  var bugidtext = bugid.innerText;
+  bugidtext = bugidtext.replace(/\s/g, '|')
+  var res  = "* {{" + bugidtext + "}} - " + bugdesc.innerHTML
+  // console.log(res)
+
+  // A replacement for GM_setClipboard
+  copyToClipboard(res);
+}
+
+function copyToClipboard(text) {
+  var txtToCopy = document.createElement('input');
+  txtToCopy.value = text;
+  document.body.appendChild(txtToCopy);
+  txtToCopy.select();
+  document.execCommand('copy');
+  txtToCopy.parentNode.removeChild(txtToCopy); 
+}
